f169f02a4333eeac4e282b390e5b5c965b33fa24
Harry Stead <harrystead9@gmail.com>
2020-12-26 08:57:56 +0000

    toggle on click

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,13 @@
 const toggleBtn = document.getElementById("navbar-toggler");
 const navDiv = document.querySelector(".navbar-collapse");
+
+toggleBtn.addEventListener("click", () => {
+    navDiv.classList.toggle("showNav");
+    if (toggleBtn.firstElementChild.className == "fas fa-bars fa-fw") {
+      toggleBtn.firstElementChild.className = "fas fa-times fa-fw";
+      document.body.style.overflow = "hidden";
+    } else {
+      toggleBtn.firstElementChild.className = "fas fa-bars fa-fw";
+      document.body.style.overflow = "visible";
+    }
+  });
